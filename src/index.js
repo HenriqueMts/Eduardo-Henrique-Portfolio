4f003f993be549511adfc8bcd466f47c78a4b9a3
HenriqueMts <henrique.mts@outlook.com.br>
2022-08-16 17:49:28 -0300

    chore: removed react-router-dom to the project

--- src/index.js
+++ src/index.js
@@ -6,14 +6,8 @@ import App from "./App";
 
 import { createRoot } from "react-dom/client";
 
-import { BrowserRouter } from "react-router-dom";
-
 const container = document.getElementById("root");
 
 const root = createRoot(container);
 
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+root.render(<App />);
